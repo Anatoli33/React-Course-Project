12b6206e348b6f3fec32dbd58716ecf7c281a194
Anatoli33 <hadjiev1215@gmail.com>
2025-11-20 10:48:00 +0200

    Editing Register form

--- new-vite-project/src/Register.jsx
+++ new-vite-project/src/Register.jsx
@@ -26,25 +26,6 @@ const Register = () => {
           <input type="password" id="confirmPassword" name="confirmPassword" required />
         </div>
 
-        <div className="form-group">
-          <label htmlFor="favoriteSport">Любим спорт</label>
-          <select id="favoriteSport" name="favoriteSport">
-            <option value="">-- Избери спорт --</option>
-            <option value="football">Футбол</option>
-            <option value="basketball">Баскетбол</option>
-            <option value="tennis">Тенис</option>
-            <option value="running">Бягане</option>
-            <option value="swimming">Плуване</option>
-          </select>
-        </div>
-
-        <div className="form-group checkbox-group">
-          <input type="checkbox" id="terms" name="terms" required />
-          <label htmlFor="terms">
-            Съгласявам се с <a href="/terms">условията</a>
-          </label>
-        </div>
-
         <button type="submit" className="btn">Регистрация</button>
 
         <p className="login-link">
